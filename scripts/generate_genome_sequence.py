7e30a2532c1da0ae4393365a737440edc859c8c8
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2020-12-25 18:04:56 -0500

    handle arguments

--- scripts/generate_genome_sequence.py
+++ scripts/generate_genome_sequence.py
@@ -9,3 +9,48 @@ and store in output_file:
   length of each genome sequence
   path to output file containing the generated sequences
 '''
+
+import random
+import argparse
+from pathlib import Path
+
+
+
+
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fasta_file", help="path to FASTA file")
+    parser.add_argument("prob_file", help="path to file for probability of confidence")
+    parser.add_argument("-n", "--num_seq", help="number of sequences to be generated")
+    parser.add_argument("-l", "--seq_length", help="length of each sequence to be generated")
+    parser.add_argument("output", help="path to output file")
+
+    args = parser.parse_args()
+
+    fasta_file = Path(args.fasta_file)
+    prob_file = Path(args.prob_file)
+
+    if args.num_seq == None:
+        num_seq = 100
+    else:
+        num_seq = int(args.num_seq)
+
+    if args.seq_length == None:
+        seq_length = 100
+    else:
+        seq_length = int(args.seq_length)
+
+    output_file = Path(args.output)
+
+    # print(fasta_file)
+    # print(prob_file)
+    # print("number", num_seq)
+    # print("length", seq_length)
+    # print(output_file)
+
+if __name__ == '__main__':
+    main()
