3b4a38dfbed3bd9f9d39182d60f35dbdc23010b6
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2020-12-25 17:31:42 -0500

    Create generate_genome_sequence.py
    add intro

---
+++ scripts/generate_genome_sequence.py
@@ -0,0 +1,11 @@
+
+'''
+Provided the following arguments, this script generates num genome sequences
+and store in output_file:
+  an FASTA file containing a genome sequence
+  a file providing the probability of confidence for each nucleotide position in
+  the FASTA file
+  number of genome sequences to be generated
+  length of each genome sequence
+  path to output file containing the generated sequences
+'''
